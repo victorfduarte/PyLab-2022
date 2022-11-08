d15f61c397cbbfd0034f2995f97aef3ffa048baa
Paulo Victor <victorfduarte18@gmail.com>
2022-11-08 18:25:22 -0300

    criando a tabela Empresa

--- empresa/models.py
+++ empresa/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    choices_nicho_mercado = (
+        ('M', 'Marketing'),
+        ('N', 'Nutrição'),
+        ('D', 'Design'),
+    )
+
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    cidade = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=30)
+    caracteristica_empresa = models.TextField()
+    nicho_mercado = models.CharField(max_length=3, choices=choices_nicho_mercado)
