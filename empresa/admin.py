44039dae87dfb0dc5cafa2b6b670aa79177dd9c2
Paulo Victor <victorfduarte18@gmail.com>
2022-11-10 12:49:17 -0300

    nova_empresa concluído

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tecnologia, Empresa, Vagas
 
-# Register your models here.
+admin.site.register(Tecnologia)
+admin.site.register(Empresa)
+admin.site.register(Vagas)
